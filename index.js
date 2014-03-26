08fdf6ad921969cb2b78039bfa1c56de06d09e80
Ionică Bizău <bizauionica@yahoo.com>
2014-03-26 13:42:11 +0200

    Added index.js file

---
+++ index.js
@@ -0,0 +1,53 @@
+// get character map
+var CharacterMap = require("./character-map");
+
+/**
+ * private: encodeCharacter
+ *  This function returns the encoded character using CharacterMap
+ *
+ *  Arguments
+ *    @input: string that must be encoded
+ *
+ *  Returns
+ *    The encoded character or @input
+ *
+ */
+function encodeCharacter (input) {
+    for (var i = 0; i < CharacterMap.length; ++i) {
+        var cCharacterMapObj = CharacterMap[i];
+        if (cCharacterMapObj.decoded === input) {
+            return cCharacterMapObj.encoded;
+        }
+    }
+    return input;
+}
+
+/**
+ * TODO
+ *
+ */
+var HTMLEncoderDecoder = {
+    decode: function (input) {
+
+    }
+    /**
+     *  TODO
+     *
+     */
+  , encode: function (input) {
+
+        // force input to be a string
+        input = String(input);
+        var output = "";
+
+        // each character
+        for (var c in input) {
+
+            // apend encoded character to output
+            output += encodeCharacter[c];
+        }
+
+        // and return the output
+        return output;
+    }
+};
