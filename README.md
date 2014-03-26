2e316c4bc9281a86837189c5361da803073e1764
Ionică Bizău <bizauionica@yahoo.com>
2014-03-26 15:50:12 +0200

    Added documentation

--- README.md
+++ README.md
@@ -2,3 +2,56 @@ HTML Decoder Encoder
 ====================
 
 HTML Encoder / Decoder - Converts characters to their corresponding HTML Entities
+
+## Installation
+
+```sh
+$ npm install html-decoder-encoder
+```
+
+## Example and testing
+
+```js
+// dependencies
+var HTMLDecoderEncoder = require("html-encoder-decoder")
+  , encoded = null
+  ;
+
+console.log(HTMLDecoderEncoder.encode(encoded = "Copyright © Ionică Bizău <bizauionica@gmail.com>"));
+console.log(HTMLDecoderEncoder.decode(encoded));
+```
+
+Running this you will get:
+
+```sh
+$ npm test
+
+> html-encoder-decoder@0.0.0 test /.../html-encoder-decoder
+> node test/1.js
+
+Copyright &copy; Ionic&#259; Biz&#259;u &lt;bizauionica@gmail.com&gt;
+Copyright © Ionică Bizău <bizauionica@gmail.com>
+```
+
+## Methods
+
+### `decode (@input)`
+Decodes an encoded string.
+
+#### Arguments
+ - @input: the encoded string
+
+#### Returns
+The decoded string.
+
+### `encode (@input)`
+Encodes a string.
+
+#### Arguments
+ - @input: the string that must be encoded
+
+#### Returns
+The encoded string.
+
+## License
+See LICENSE file.
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+/**
+ *
+ *  HTML Encoder / Decoder
+ *  Converts characters to their corresponding HTML Entities",
+ *
+ *  Licensed under MIT license
+ *
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2014 Ionică Bizău
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ */
+
 // get character map
 var CharacterMap = require("./character-map");
 
@@ -22,11 +53,20 @@ function encodeCharacter (input) {
     return input;
 }
 
-/**
- * TODO
- *
- */
+// constructor
 var HTMLEncoderDecoder = {
+
+    /**
+     *  HTMLEncoderDecoder#decode
+     *    Decodes an encoded string.
+     *
+     *    Arguments
+     *      @input: the encoded string
+     *
+     *    Returns
+     *      The decoded string.
+     *
+     */
     decode: function (input) {
 
         // force input to be a string
@@ -39,8 +79,16 @@ var HTMLEncoderDecoder = {
 
         return output;
     }
+
     /**
-     *  TODO
+     *  HTMLEncoderDecoder#encode
+     *    Encodes a string.
+     *
+     *    Arguments
+     *      @input: the string that must be encoded
+     *
+     *    Returns
+     *      The encoded string.
      *
      */
   , encode: function (input) {
